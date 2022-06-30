86443efaac43aa89b1cb902e8c3d692b12ec8fa1
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2022-06-30 18:54:10 +0200

    db seed covered

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Cour.create(id:2, package:"Privé", durée:"4 heures / 2x 2 heures", prix:"60 Euro")
+# Cour.create(id:3, package:"Privé", durée:"10 heures / 5x 2 heures", prix:"150 Euro")
+# Cour.create(id:4, package:"Semi-privé", durée:"4 heures / 2x 2 heures", prix: "40 Euro")
+# Cour.create(id:5, package:"Semi-privé", durée:"10 heures / 5x 2 heures", prix: "100 Euro" )
+# Cour.create(id:6, package:"Groupe", durée:"4 heures / 2x 2 heures", prix: "30 Euro")
+# Cour.create(id:7, package:"Groupe", durée:"10 heures / 5x 2 heures", prix: "80 Euro")
+
+
 # Client.create(nom:"fred", pseudo:"fredo", mail:"fred@gmail.fr", client_id:1, adresse: "5 rue du jardin", interets: "manga, commics, sport,  mobilier gamer", age: 19)
 # Client.create(nom: "Van Dame", pseudo: "JCVD", mail: "leauCestBon@hotmail.fr", client_id: 3, adresse: "belgique", interets: "jardin, piscine, bricolage" ,age: 57)
 # Client.create(nom: "Bourgeois", pseudo:"Docteur Bourgeois", mail:"florent.bourgeois@uha.fr", client_id:4, adresse: "UHA 4.0", interets:" jardin, bricolage, cuisine", age: 666)
 # Client.create(nom: "Dickinson", pseudo:"The trooper", mail:"singer@ironMaiden.uk", client_id:5, adresse: "Strange World", interets:"musique, japon, rock ,piscine" , age:59)
 # Client.create(nom: "edward", pseudo: "ed", mail: "fullMetalAlchemist@uha.fr", client_id: 2, adresse: "10 rue de l'homonculus ; amestris", interets: "alchimie, pierre philosophale, armures, sport", age:14)
-# Commande.create(id:1, client_id:1, date:"25/10/2018", Cources:"doublon + chaine Hi-Fi + Voiture téléguidée + pistolet à eau", date_de_livraison:"29/10/2018", payement:"carte bancaire", enseigne:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Ikea_logo.svg/249px-Ikea_logo.svg.png")
+
+# Commande.create(id:1, client_id:1, cour_id: 1, date_de_reservation:"29/10/2022", payement:"carte bancaire")
+# Commande.create(id:2, client_id:2, cour_id: 2, date_de_reservation:"23/06/2023", payement:"virement")
+# Commande.create(id:3, client_id:3, cour_id: 3, date_de_reservation:"20/04/2023", payement:"carte member")
+# Commande.create(id:4, client_id:4, cour_id: 4, date_de_reservation:"20/07/2023", payement:"carte bancaire")
